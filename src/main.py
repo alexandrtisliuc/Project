c85bf8dd62eaca0169f6f72ac611c8643bda2bab
alexandrtisliuc <alex20096825@gmail.com>
2025-05-02 18:43:34 +0300

    requirements steps

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/home')
-async def read_root():
-    return {'Hello': 'World'}
+@app.post("/home")
+def read_root():
+    return {"Hello": "World"}
